7b25d5debc63ab2eb169a1c29d6e298c24f804ba
lmaxxor <111413261+lmaxxor@users.noreply.github.com>
2023-03-21 22:06:51 -0300

    Update _banner.sh

--- utils/_banner.sh
+++ utils/_banner.sh
@@ -23,16 +23,16 @@ print_banner() {
   printf "\n\n"
 
   printf "${BLUE}";
-  printf "                 ████████   ██    ██   ████████   ████████   ████████   ████████   \n";
-  printf "                 ██          ██  ██    ██              ███   ██    ██   ██    ██   \n";
-  printf "                 ████████     ████     ████████      ███     ████████   ████████   \n";
-  printf "                       ██      ██            ██   ███        ██    ██   ██          \n";
-  printf "                 ████████      ██      ████████   ████████   ██    ██   ██           \n";
-  printf "                               SYSZAP - SISTEMA DE MULTIATENDIMENTO                    \n";
-  printf "                                        www.syszap.com.br                             \n";
+  printf "                 ██      ██   ████████   ██   ██   ██      ██   ████████   \n";
+  printf "                 ██      ██       ██      ██   ██   ██      ██       ██      \n";
+  printf "                 ██      ██       ██      ███████   ██      ██       ██      \n";
+  printf "                 ██      ██       ██      ██   ██   ██      ██       ██      \n";
+  printf "                 ████████    ████████   ██   ██   ████████    ████████   \n";
+  printf "                                    IOI Digital - SISTEMA DE MULTIATENDIMENTO                    \n";
+  printf "                                             www.ioidigital.com.br                             \n";
   printf "\n"
  
   printf "${NC}";
 
   printf "\n"
-}
+}"
